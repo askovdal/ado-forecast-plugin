60e864b6bb403ab46bbe818576ce43b945ab6f0b
Andreas Skovdal <andreas@adaptagency.com>
2023-11-21 16:00:38 +0100

    Add links to related tasks as well

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+const urls = new Map();
+
 const main = async () => {
   const { token } = await import(chrome.runtime.getURL('token.js'));
   const observer = new MutationObserver((ms) => mutationCallback(ms, token));
@@ -10,49 +12,100 @@ const mutationCallback = (mutations, token) => {
       continue;
     }
 
-    const items = mutation.target.querySelectorAll('.work-item-form-page');
-    for (const item of items) {
-      if (item.hasAttribute('data-ado-forecast')) {
-        continue;
-      }
-
-      item.setAttribute('data-ado-forecast', '');
-
-      const projectName = item.querySelector('#__bolt--Area-input').value;
-      const itemHeader = item.querySelector('.work-item-form-header');
-      const taskElements = itemHeader.querySelector('a').innerText.split(' ');
-      const taskId = taskElements.pop();
-      const taskType = taskElements.join(' ');
-
-      if (!['TASK', 'BUG'].includes(taskType)) {
-        continue;
-      }
-
-      getForecastUrl(token, taskId, projectName).then(
-        ({ url }) => insertForecastLink(itemHeader, url),
-        console.log,
-      );
-    }
+    iterateItems(mutation, token);
+    iterateRelated(mutation, token);
   }
 };
 
-const getForecastUrl = (token, taskId, projectName) =>
-  fetch(
+const iterateItems = ({ target }, token) => {
+  const items = target.querySelectorAll('.work-item-form-page');
+  for (const item of items) {
+    if (!elementIsNew(item)) {
+      continue;
+    }
+
+    const itemHeader = item.querySelector('.work-item-form-header');
+    const taskElements = itemHeader.querySelector('a').innerText.split(' ');
+    const taskId = taskElements.pop();
+    const taskType = taskElements.join(' ');
+    const projectName = item.querySelector('#__bolt--Area-input').value;
+
+    setLink(taskType, token, taskId, projectName, false, (forecastLink) => {
+      itemHeader.querySelector('.flex-row.flex-noshrink').after(forecastLink);
+    });
+  }
+};
+
+const iterateRelated = ({ target }, token) => {
+  const relatedList = target.querySelectorAll('.artifact-link-container');
+  for (const related of relatedList) {
+    if (!elementIsNew(related)) {
+      continue;
+    }
+
+    const taskType = related.querySelector('[role=img]').ariaLabel;
+    const taskId = related.querySelector('.artifact-link-id').innerText;
+    const projectName = related
+      .closest('.work-item-form-page')
+      .querySelector('#__bolt--Area-input').value;
+
+    setLink(taskType, token, taskId, projectName, true, (forecastLink) => {
+      related
+        .querySelector('.padding-right-8.text-ellipsis')
+        .after(forecastLink);
+    });
+  }
+};
+
+const elementIsNew = (element) => {
+  if (element.hasAttribute('data-ado-forecast')) {
+    return false;
+  }
+  element.setAttribute('data-ado-forecast', '');
+  return true;
+};
+
+const setLink = (taskType, token, taskId, projectName, related, insert) => {
+  if (!shouldLogTimeOnTask(taskType)) {
+    return;
+  }
+
+  getForecastUrl(token, taskId, projectName).then((url) => {
+    const forecastLink = createForecastLink(url, related);
+    insert(forecastLink);
+  }, console.log);
+};
+
+const shouldLogTimeOnTask = (taskType) =>
+  ['task', 'bug'].includes(taskType.toLowerCase());
+
+const getForecastUrl = async (token, taskId, projectName) => {
+  const savedUrl = urls.get(taskId);
+  if (savedUrl) {
+    return savedUrl;
+  }
+
+  const response = await fetch(
     `https://ado-forecast.onrender.com/?task_id=${taskId}&project_name=${projectName}`,
     { headers: { Authorization: `Bearer ${token}` } },
-  ).then((response) => response.json());
+  );
 
-const insertForecastLink = (itemHeader, url) => {
+  const { url } = await response.json();
+  urls.set(taskId, url);
+  return url;
+};
+
+const createForecastLink = (url, related) => {
   const forecastLogo = document.createElement('img');
   forecastLogo.src = chrome.runtime.getURL('forecast-logo.png');
 
   const forecastLink = document.createElement('a');
   forecastLink.href = url;
   forecastLink.target = '_blank';
-  forecastLink.classList.add('ado-forecast');
+  forecastLink.classList.add('ado-forecast', related && 'related');
   forecastLink.append(forecastLogo);
 
-  itemHeader.querySelector('.flex-row .flex-noshrink').after(forecastLink);
+  return forecastLink;
 };
 
 main();
